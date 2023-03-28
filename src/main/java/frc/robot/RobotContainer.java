005d7bfc7da9412b5d3d2c9ed08018e12317a8da
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-03-28 12:28:59 -0300

    :sparkles: feat: Add Swerve Command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.OIConstants;
+import frc.robot.commands.SwerveJoystickCmd;
+import frc.robot.subsystems.SwerveSubsystem;
 
 
 public class RobotContainer {
 
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+
+  private final Joystick driverJoystick = new Joystick(OIConstants.kDriverControllerPort);
+
   public RobotContainer() {
+    swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
+      swerveSubsystem, 
+      () -> -driverJoystick.getRawAxis(OIConstants.kDriverYAxis), 
+      () -> driverJoystick.getRawAxis(OIConstants.kDriverXAxis), 
+      () -> driverJoystick.getRawAxis(OIConstants.kDriverRotAxis), 
+      () -> !driverJoystick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx)));
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    // Reset the robot heading
+    new JoystickButton(driverJoystick, 2).onTrue(new InstantCommand(() -> swerveSubsystem.zeroHeading()));
+  }
   
   public Command getAutonomousCommand() {
     return null;
